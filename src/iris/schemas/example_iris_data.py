0ff69d241fdf53659e1344c77130038c14bcb7b6
Nok Chan <nok.lam.chan@quantumblack.com>
2023-08-25 01:52:42 +0100

    update schema
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- src/iris/schemas/example_iris_data.py
+++ src/iris/schemas/example_iris_data.py
@@ -1,4 +1,7 @@
 import pandera as pa
+from pandera.typing import Series
 
 class ExampleIrisDataSchema(pa.DataFrameModel):
     sepal_length: Series[int] = pa.Field(gt=2000)
+
+schema = ExampleIrisDataSchema
